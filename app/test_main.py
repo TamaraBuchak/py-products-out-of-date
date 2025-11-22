13e8e5d68d6a1047593c0dacc361361cef6cba9b
Buchak Tamara <Buchak.Tamara@gmail.com>
2025-11-22 21:08:05 +0200

    fix indents

--- app/test_main.py
+++ app/test_main.py
@@ -7,61 +7,64 @@ from app.main import outdated_products
 @pytest.mark.parametrize(
     "products_dict, expected_result",
     [
-        pytest.param([
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2022, 2, 10),
-                "price": 600
-            },
-            {
-                "name": "chicken",
-                "expiration_date": datetime.date(2022, 2, 5),
-                "price": 120
-            },
-            {
-                "name": "duck",
-                "expiration_date": datetime.date(2022, 2, 3),
-                "price": 160
-            }
-        ],
+        pytest.param(
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 3),
+                    "price": 160
+                }
+            ],
             [],
             id="should return empty list"),
-        pytest.param([
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2022, 2, 10),
-                "price": 600
-            },
-            {
-                "name": "chicken",
-                "expiration_date": datetime.date(2022, 2, 2),
-                "price": 120
-            },
-            {
-                "name": "duck",
-                "expiration_date": datetime.date(2022, 2, 1),
-                "price": 160
-            }
-        ],
+        pytest.param(
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 2),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
             ["duck"],
             id="should check product with expiration date equals today"),
-        pytest.param([
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2022, 1, 10),
-                "price": 600
-            },
-            {
-                "name": "chicken",
-                "expiration_date": datetime.date(2022, 1, 21),
-                "price": 120
-            },
-            {
-                "name": "duck",
-                "expiration_date": datetime.date(2022, 2, 1),
-                "price": 160
-            }
-        ],
+        pytest.param(
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 1, 10),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 1, 21),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                }
+            ],
             ["salmon", "chicken", "duck"],
             id="should check products out of date"),
     ]
